d0b46504828bb8fda16ef32e71e02b650502e2e3
alwick <awick@reinventauctions.com>
2023-12-03 20:42:01 -0600

    Directly use rust.

--- data-generator/combine_data.sh
+++ data-generator/combine_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 python3 combineOrganizations.py
+mkdir ../data
 mv ./data/meetup/combined.json ../data/
--- data-generator/meetup/fetch_and_store.sh
+++ data-generator/meetup/fetch_and_store.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /greetup/meetup/meetups.csv | /greetup/meetup/store_data.sh
+cat ./meetups.csv | ./store_data.sh
