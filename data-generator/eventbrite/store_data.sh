7f5694321a13f75dd75a59c1a8ef2c0be222ba04
alwick <awick@reinventauctions.com>
2023-12-29 15:36:01 -0600

    Add support for eventbrite images.

--- data-generator/eventbrite/store_data.sh
+++ data-generator/eventbrite/store_data.sh
@@ -12,6 +12,7 @@ while IFS= read -r line; do
 	script_tags=$(echo $response | hq '{scripts: script[type="application/ld+json"]  | [@text]}')
 	attr_tags=[$(echo $response | hq '{title: meta[property="og:title"]  | @(content)}'),
 	attr_tags+=$(echo $response | hq '{description: meta[property="og:description"]  | @(content)}'),
+	attr_tags+=$(echo $response | hq '{image: meta[property="og:image"]  | @(content)}'),
 	attr_tags+=$(echo $response | hq '{url: meta[property="og:url"] | @(content)}')]
 	organizations=$(echo $attr_tags | xq 'add | [.]')
 	events=$(echo $script_tags | xq '.scripts[] | [fromjson] ')
