10d219797ec463fe293ed88bdbdc8d9e584de7d4
Optiq01 <Optiqvision@gmail.com>
2024-06-29 21:33:04 -0500

    create footer component

---
+++ web/src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as styles from './Footer.module.css';
+
+export const Footer = ()=>{
+    return(
+        <footer>
+            <p>footer</p>
+        </footer>
+    );
+}
