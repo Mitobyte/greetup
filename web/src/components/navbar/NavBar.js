b4a7bc2dcd016b02153f8f5e3b2df3c1e38ee0e4
Optiq01 <Optiqvision@gmail.com>
2024-06-29 21:22:39 -0500

    build navbar

--- web/src/components/navbar/NavBar.js
+++ web/src/components/navbar/NavBar.js
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import * as styles from './NavBar.module.css';
+import {Link} from 'gatsby';
 
 export const NavBar = ()=>{
     return(
-        <nav>
-            
+        <nav className={`srcryBox ${styles.navBar}`}>
+
+            <svg className={styles.icon} viewBox="0 0 195.899414 181.036621" >
+                <use href="#m_icon" />
+            </svg>
+
+            <ul className={styles.linkList}>
+                <li className={styles.linkContainer}>
+                    <Link className={styles.link} to="/" activeClassName={styles.active}>Home</Link>
+                </li>
+                <li className={styles.linkContainer}>
+                    <Link className={styles.link} to="/calendar" activeClassName={styles.active}>Calendar</Link>
+                </li>
+            </ul>
+
         </nav>
     )
 }
