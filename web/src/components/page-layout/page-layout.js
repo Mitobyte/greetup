7e62e30ad74fce3fc9c5fe7674be6e63852e783e
Optiq01 <Optiqvision@gmail.com>
2024-07-01 23:07:53 -0500

    remove unused code

--- web/src/components/page-layout/page-layout.js
+++ web/src/components/page-layout/page-layout.js
@@ -1,26 +1,20 @@
 import * as React from 'react';
-import * as styles from './page-layout.module.css';
+import './page-layout.css';
 import { SvgSymbols } from '../svg_symbols/SvgSymbols';
 import { NavBar } from '../navbar/NavBar';
 import { Footer } from '../footer/Footer';
 
 export const PageLayout = ({data, children})=>{
     return(
-        <section className={styles.pageContainer}>
+        <section>
             <SvgSymbols />
-            <article className={styles.navBar}>
+            <article className="navBarContainer">
                 <NavBar />
             </article>
             <main>
                 {children}
             </main>
             <Footer />
-            {/*<svg style={{width: 'var(--small-1-3)'}}>
-                <use href="#full_logo" style={{'--color01': 'hsl(var(--yellow-200), 1)', '--color02': 'hsl(var(--blue-900), 1)'}} width="100%" />
-            </svg>
-            <p className="paragraph" style={{fontSize: 'var(--text-title-3)'}}>
-                lorem ipsum blahblahblahblah
-            </p>*/}
         </section>
     );
 }
