dedc1d552431d5480ccf7f53da4d44421cdc2940
Optiq01 <Optiqvision@gmail.com>
2024-07-01 23:05:55 -0500

    markup and style hero component

--- web/src/components/hero/Hero.js
+++ web/src/components/hero/Hero.js
@@ -1,4 +1,22 @@
 import * as React from 'react';
 import * as styles from './Hero.module.css';
 
-export const Hero = ()=>{}
+export const Hero = ()=>{
+    return(
+        <section className={styles.mainContainer}>
+            <div className={styles.backgroundPhoto}></div>
+            <div className={styles.backgroundFilter}></div>
+            <div className={styles.backgroundGradient}></div>
+
+            <article className={styles.contentContainer}>
+                <svg className={styles.logo}>
+                    <use href="#full_logo" style={{'--color01': 'hsl(var(--yellow-200), 1)', '--color02': 'hsl(var(--blue-900), 1)'}} width="100%" height="100%" />
+                </svg>
+
+                <p className={`srcryTxt ${styles.introText}`}>
+                    Milwaukee is home to a vibrant and thriving tech community. This website is dedicated to showcasing upcoming events so you can stay in the loop and get involved with whatever interests you. 
+                </p>
+            </article>
+        </section>
+    );
+}
