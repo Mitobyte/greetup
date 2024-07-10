b511ec65a3990351fa6ab2ad6606c47f9c915a45
Optiq01 <Optiqvision@gmail.com>
2024-07-09 23:16:54 -0500

    markup and style event list component

--- web/src/components/event-list/EventList.js
+++ web/src/components/event-list/EventList.js
@@ -1,8 +1,34 @@
 import * as React from 'react';
 import * as styles from './EventList.module.css';
+import {Location} from './location/Location';
+import { DateComponent } from './date/DateComponent';
 
-export const EventList = ({data}) =>{
+export const EventList = ({data, key}) =>{
     return(
-        <section></section>
+        <ul className={styles.mainContainer}>
+            {
+                data.map((item, index)=>{
+                    return(
+                        <li className={styles.listItem}>
+                            <h3 className={`srcryTxt ${styles.eventName}`}>{item.name}</h3>
+                            <Location data={item.location} />
+                            <DateComponent start={item.startDate} end={item.endDate} />
+                            {/*
+                                item.description &&
+                                <article className={styles.descriptionContainer}>
+                                    <h6 className={`srcryTxt ${styles.descriptionTitle}`}>Description</h6>
+                                    <p className={`srcryTxt ${styles.descriptionText}`}>
+                                        {item.description}
+                                    </p>
+                                </article>
+                            */}
+                            <a className={`srcryTxt ${styles.joinButton}`} href={item.url} target="_blank">
+                                Join Event
+                            </a>
+                        </li>
+                    );
+                })
+            }
+        </ul>
     );
 }
