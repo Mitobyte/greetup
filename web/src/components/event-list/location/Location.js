74aa0924dd69cdfeab015fd7f6d0fdeeadf16b65
Optiq01 <Optiqvision@gmail.com>
2024-07-09 23:15:32 -0500

    markup and style location component

--- web/src/components/event-list/location/Location.js
+++ web/src/components/event-list/location/Location.js
@@ -1,8 +1,19 @@
 import * as React from 'react';
 import * as styles from './Location.module.css';
 
-export const Location = (data)=>{
+export const Location = ({data})=>{
     return(
-        <></>
+        <section className={styles.mainContainer}>
+            <h6 className={`srcryTxt ${styles.title}`}>Location</h6>
+            {data['@type'] === 'VirtualLocation' &&
+                <p className={`srcryTxt ${styles.locationText}`}>Online</p>
+            }
+            {
+                data['@type'] === 'Place' &&
+                <p className={`srcryTxt ${styles.locationText}`}>
+                    <b>{data.name}</b> {data.address.streetAddress}
+                </p>
+            }
+        </section>
     )
 }
