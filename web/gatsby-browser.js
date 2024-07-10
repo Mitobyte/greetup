39fef82c33889039fbdb9718638888e62d215ab9
Optiq01 <Optiqvision@gmail.com>
2024-07-09 23:30:56 -0500

    correct filename and comment code for clean slate

---
+++ web/gatsby-browser.js
@@ -0,0 +1,12 @@
+import './src/styles/global.css';
+/*import React from "react";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './src/theme.js';*/
+
+/*export const wrapRootElement = ({ element }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {element}
+    </ThemeProvider>
+  );
+}*/
