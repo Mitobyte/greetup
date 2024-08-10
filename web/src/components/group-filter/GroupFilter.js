c6643390203caa3698fcf1577a157255d0173455
Optiq01 <Optiqvision@gmail.com>
2024-08-10 00:07:23 -0500

    create group filter component

---
+++ web/src/components/group-filter/GroupFilter.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as styles from './GroupFilter.module.css';
+
+export const GroupFilter = ({nameList, resultList}) =>{
+    return(
+        <p>Group Filter</p>
+    );
+}
