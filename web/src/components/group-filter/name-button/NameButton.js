9922dbe8cb26ab2b25293a27790272702259b9a8
Optiq01 <Optiqvision@gmail.com>
2024-08-15 16:06:37 -0500

    add light indicator and style name button

--- web/src/components/group-filter/name-button/NameButton.js
+++ web/src/components/group-filter/name-button/NameButton.js
@@ -3,8 +3,9 @@ import * as styles from './NameButton.module.css';
 
 export const NameButton = ({name, selected, active})=>{
     return(
-        <span>
+        <span className={`srcryBox ${styles.nameButton}`}>
             {name}
+            <div className={styles.indicator}></div>
         </span>
     );
 }
