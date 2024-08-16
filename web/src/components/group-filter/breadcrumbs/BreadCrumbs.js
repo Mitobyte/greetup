1770b830a28214cd3de10746af6946c78ad7671f
Optiq01 <Optiqvision@gmail.com>
2024-08-15 23:30:09 -0500

    create a component for breadcrumbs

---
+++ web/src/components/group-filter/breadcrumbs/BreadCrumbs.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as styles from './BreadCrumbs.module.css';
+
+export const BreadCrumbs = ({crumbs, removeGroup}) => {
+    return(
+        <p>crumbs</p>
+    );
+}
---
+++ web/src/components/group-filter/breadcrumbs/crumb/Crumb.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as styles from './Crumb.module.css';
+
+export const Crumb = ({crumb, removeGroup}) => {
+    return(
+        <span>crumb</span>
+    );
+}
