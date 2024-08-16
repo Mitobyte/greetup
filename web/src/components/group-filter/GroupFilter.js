94f1fb205c9335683a9e7b45af9e10538e58bec9
Optiq01 <Optiqvision@gmail.com>
2024-08-15 23:18:43 -0500

    handle logic for updating list of selected groups

--- web/src/components/group-filter/GroupFilter.js
+++ web/src/components/group-filter/GroupFilter.js
@@ -4,14 +4,40 @@ import { NameButton } from './name-button/NameButton';
 
 export const GroupFilter = ({nameList, resultList}) =>{
 
+    const [filteredNames, setFilteredNames] = React.useState([]);
+    const [selectedNames, setSelectedNames] = React.useState(['all']);
+
+    React.useEffect(()=>{ setFilteredNames(nameList)}, [nameList]);
+    
+    const handleSelection = (name)=>{
+
+        name === 'all' ? setSelectedNames(['all']) :
+        selectedNames.indexOf('all') > -1 ? setSelectedNames([name]) :
+        selectedNames.indexOf(name) > -1 && selectedNames.length === 1 ? setSelectedNames(['all']) :
+        selectedNames.indexOf(name) > -1 ? setSelectedNames([...selectedNames.filter(a=> a !== name)]) :
+        setSelectedNames([...selectedNames, name]);
+    }
+
     return(
-        <article>
+        <article className={styles.mainContainer}>
             <p>
                 {
-                    nameList.map((name, index)=>
+                    selectedNames.map((name)=> <span>{name}</span>)
+                }
+            </p>
+            <p className={styles.nameContainer}>
+                <NameButton
+                    name={'all'}
+                    active={selectedNames.indexOf('all') > -1 ? true : false}
+                    selected={(a)=> handleSelection(a)}
+                />
+                {
+                    filteredNames.map((name, index)=>
                         <NameButton
-                            id={`company_name_btn_${index}`}
+                            key={`company_name_btn_${index}`}
                             name={name}
+                            active={selectedNames.indexOf(name) > -1 ? true : false}
+                            selected={(a)=> handleSelection(a)}
                         />
                     )
                 }
