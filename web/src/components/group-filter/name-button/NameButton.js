3a241751109fcbfb97f091247a7551c359264098
Optiq01 <Optiqvision@gmail.com>
2024-08-15 23:11:30 -0500

    add logic to toggle active and inactive states and update list of selected groups

--- web/src/components/group-filter/name-button/NameButton.js
+++ web/src/components/group-filter/name-button/NameButton.js
@@ -3,9 +3,34 @@ import * as styles from './NameButton.module.css';
 
 export const NameButton = ({name, selected, active})=>{
     return(
-        <span className={`srcryBox ${styles.nameButton}`}>
+        <span
+            className={`srcryBox ${styles.nameButton}`}
+            onClick={()=> selected(name)}
+            style={active?
+                    {
+                        backgroundColor : 'var(--active-background)',
+                        color : 'var(--active-color)'
+                    }
+                    :
+                    {
+                        backgroundColor : 'var(--inactive-background)',
+                        color : 'var(--inactive-color)'
+                    }
+                }
+        >
             {name}
-            <div className={styles.indicator}></div>
+            <div
+                className={styles.indicator}
+                style={active? 
+                    {
+                        backgroundColor : 'var(--active)'
+                    }
+                    :
+                    {
+                        backgroundColor : 'var(--inactive)'
+                    }
+                }
+            ></div>
         </span>
     );
 }
