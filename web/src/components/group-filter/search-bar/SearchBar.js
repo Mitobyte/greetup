ccb63591b7e15bc52ebbc305b9e8ab80fd44d1f2
Optiq01 <Optiqvision@gmail.com>
2024-08-17 03:25:19 -0500

    markup and style search bar

--- web/src/components/group-filter/search-bar/SearchBar.js
+++ web/src/components/group-filter/search-bar/SearchBar.js
@@ -3,6 +3,16 @@ import * as styles from './SearchBar.module.css';
 
 export const SearchBar = ({searchText})=>{
     return(
-        <p>search bar</p>
+        <article className={styles.mainContainer}>
+            <label for="searchBar" className={`srcryBox ${styles.searchLabel}`}>
+                <input
+                    id="searchBar"
+                    type="search"
+                    className={styles.searchBar}
+                    placeholder="search for groups"
+                />
+                <button type="button" className={styles.searchButton}>search</button>
+            </label>
+        </article>
     );
 }
