229a9c3f7214f75f13a510487a624761f7e9d643
Optiq01 <Optiqvision@gmail.com>
2024-08-19 03:25:43 -0500

    create logic for loading filtered names

--- web/src/components/group-filter/name-container/NameContainer.js
+++ web/src/components/group-filter/name-container/NameContainer.js
@@ -2,24 +2,30 @@ import * as React from 'react';
 import * as styles from './NameContainer.module.css';
 import { NameButton } from './name-button/NameButton';
 
-export const NameContainer = ({names, selected, updateList}) =>{
+export const NameContainer = ({names, selectedGroups, filteredGroups, updateList}) =>{
     return(
+        <article className={styles.mainContainer}>
+
         <p className={styles.nameContainer}>
                 <NameButton
                     name={'all'}
-                    active={selected.indexOf('all') > -1 ? true : false}
+                    active={selectedGroups.indexOf('all') > -1 ? true : false}
                     selected={(a)=> updateList(a)}
                 />
-                {
+                { filteredGroups.filter(a => a !== 'all').length > 0 ?
                     names.map((name, index)=>
                         <NameButton
                             key={`company_name_btn_${index}`}
                             name={name}
-                            active={selected.indexOf(name) > -1 ? true : false}
+                            active={selectedGroups.indexOf(name) > -1 ? true : false}
                             selected={(a)=> updateList(a)}
                         />
                     )
+                    :
+                    <span className={styles.noResultText}>(no results)</span>
+                    
                 }
             </p>
+        </article>
     );
 }
