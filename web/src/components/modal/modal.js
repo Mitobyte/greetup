3078ef25ddb7bea7edfe55592bf29f87a48e6a8e
Optiq01 <Optiqvision@gmail.com>
2024-08-29 23:04:09 -0500

    formate code in modal

--- web/src/components/modal/modal.js
+++ web/src/components/modal/modal.js
@@ -1,17 +1,29 @@
 import * as React from 'react';
 import * as styles from './modal.module.css';
 
-export const Modal = ({children, toggle})=>{
+export const Modal = ( { children, toggle } ) => {
+
     return(
-        <section className={styles.mainContainer}>
-            <article className={`srcryBox ${styles.contentContainer}`}>
-                <svg className={`srcryBox ${styles.closeButton}`} viewBox="0 0 20 20" onClick={()=> toggle(false)}>
+        <section className={ styles.mainContainer }>
+
+            <article className={ `srcryBox ${ styles.contentContainer }` }>
+
+
+                <svg
+                    className={ `srcryBox ${ styles.closeButton }` }
+                    viewBox="0 0 20 20"
+                    onClick={ () => toggle( false ) }
+                >
                     <use href="#close_button" />
                 </svg>
-                <article className={styles.projectionContainer}>
-                    {children}
+
+                <article className={ styles.projectionContainer }>
+                    { children }
                 </article>
+
+                
             </article>
+
         </section>
     );
 }
