6996ea7cacc0e142f960bffda31c381746726def
Optiq01 <Optiqvision@gmail.com>
2024-08-29 23:01:11 -0500

    change colors of buttons on hover

--- web/src/components/organization-card/OrganizationCard.js
+++ web/src/components/organization-card/OrganizationCard.js
@@ -1,36 +1,56 @@
 import * as React from 'react';
 import * as styles from './OrganizationCard.module.css';
 
-export const OrganizationCard = ({data, selection})=>{
+export const OrganizationCard = ( { data, selection } ) => {
 
     return(
-        <article className={styles.mainContainer}>
-            <img src={data.image} alt="Group Logo" className={styles.companyLogo} />
-            <h3 className={`srcryTxt ${styles.companyName}`}>{data.name}</h3>
+        <article className={ styles.mainContainer }>
+
+
+            
+            <img src={ data.image } alt="Group Logo" className={ styles.companyLogo } />
+
+            <h3 className={ `srcryTxt ${styles.companyName}` }>{ data.name }</h3>
+
             {
                 data.events.length < 1 &&
-                <p className={`srcryTxt ${styles.noEventText}`}>
+
+                <p className={ `srcryTxt ${ styles.noEventText }` }>
                     <i>no upcoming events at this time</i>
                 </p>
             }
+
             {
                 data.events.length > 0 &&
-                <article className={styles.eventContainer}>
-                    <p className={`srcryTxt ${styles.eventText}`}>
-                        <span className={styles.eventCount}>{data.events.length}</span> upcoming event{data.events.length > 1 ? 's': ''}!!!
+
+                <article className={ styles.eventContainer }>
+
+
+                    <p className={ `srcryTxt ${ styles.eventText }` }>
+                        <span className={ styles.eventCount }>{ data.events.length }</span> upcoming event{ data.events.length > 1 ? 's' : '' }!!!
                     </p>
 
                     <button
-                        className={`srcryTxt ${styles.viewButton}`}
-                        onClick={()=> selection(data.events)}
+                        className={ `srcryTxt ${ styles.buttonCore } ${ styles.viewButton }` }
+                        onClick={ () => selection( data.events ) }
                     >
                         view events
                     </button>
+
+                    
                 </article>
             }
-            <a className={`srcryTxt ${styles.visitButton}`} target="_blank" href={data.url}>
+
+            <a
+                className={ `srcryTxt ${ styles.buttonCore } ${ styles.visitButton }` }
+                target="_blank"
+                href={ data.url }
+            >
                 learn more
             </a>
+
+
+
         </article>
     );
 }
