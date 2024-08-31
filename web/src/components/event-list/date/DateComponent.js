a05c7509ce435c9e69bf55e00f95a34f23fdfdb5
Optiq01 <Optiqvision@gmail.com>
2024-08-31 02:00:33 -0500

    clean and comment code in DateComponent

--- web/src/components/event-list/date/DateComponent.js
+++ web/src/components/event-list/date/DateComponent.js
@@ -1,19 +1,40 @@
-import * as React from 'react';
-import * as styles from './DateComponent.module.css';
+import * as React       from 'react';
+import * as styles      from './DateComponent.module.css';
 import * as DateHelpers from '../../../utils/date-helpers';
+
+
+
+
+
+
 
 export const DateComponent = ( { start, end } ) => {
 
+      //  Date the event starts --->
     const startDate = new Date( start );
+
+      //  Date the event ends --->
     const endDate   = new Date( end );
+
+      //  Name of the day of the week --->
     const weekDay   = DateHelpers.getWeekDay( startDate.getDay() );
+
+      //  Name of the month --->
     const month     = DateHelpers.getMonth( startDate.getMonth() );
+
+      //  Number date --->
     const day       = DateHelpers.getDay( startDate.getDay() );
+
+      //  Time the event starts --->
     const startTime = DateHelpers.getTime( startDate.getHours(), startDate.getMinutes() );
+
+      //  Time the event ends --->
     const endTime   = DateHelpers.getTime( endDate.getHours(), endDate.getMinutes() );
 
 
     
+
+
     return(
 
         <section className={ styles.mainContainer }>
@@ -23,7 +44,7 @@ export const DateComponent = ( { start, end } ) => {
             <p className={ `srcryTxt ${ styles.dateText }` }>
 
                 { weekDay } { month } { day }, { startDate.getFullYear() }<br />
-                
+
                 <b>{ startTime } - { endTime }</b>
 
             </p>
