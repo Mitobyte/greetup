743ee2d89ed521750d5491d8e242b9827b87bb6c
Optiq01 <Optiqvision@gmail.com>
2024-08-31 02:23:22 -0500

    clean up code in breadcrumbs component

--- web/src/components/group-filter/breadcrumbs/BreadCrumbs.js
+++ web/src/components/group-filter/breadcrumbs/BreadCrumbs.js
@@ -1,58 +1,51 @@
-import * as React from 'react';
+import * as React  from 'react';
 import * as styles from './BreadCrumbs.module.css';
-import { Crumb } from './crumb/Crumb';
-import { AnimatePresence, LayoutGroup, motion, useIsPresent } from 'framer-motion';
+import { Crumb }   from './crumb/Crumb';
+import { AnimatePresence, LayoutGroup, motion } from 'framer-motion';
 
-export const BreadCrumbs = ({crumbs, removeGroup}) => {
 
-    const isPresent = useIsPresent();
+
+
+
+
+
+export const BreadCrumbs = ( { crumbs, removeGroup } ) => {
+    
     return(
-        <article style={{width: '100%'}}>
+
+        <article style={{ width : '100%' }}>
+
+
             <AnimatePresence mode="popLayout">
+                
                 <LayoutGroup>
                 {
-                    crumbs.map((item, index)=>
+                    crumbs.map( ( item, index ) =>
+
                         <motion.div
-                            className={styles.crumbAnimationContainer}
-                            key={`crumb_${index}`}
+                            className={ styles.crumbAnimationContainer }
+                            key={ `crumb_${ index }` }
                             layout="preserve-aspect"
-                            initial={{ transform: 'translateX(120vw)' }}
-                            animate={{ transform: 'translateX(0)' }}
+                            initial={{ transform : 'translateX(120vw)' }}
+                            animate={{ transform : 'translateX(0)' }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.3 }}
                         >
-                            {<Crumb
-                                crumb={item}
-                                deleteCrumb={(crumb)=> removeGroup(crumb)}
-                            />}
+
+                            <Crumb
+                                crumb={ item }
+                                deleteCrumb={ ( crumb ) => removeGroup( crumb ) }
+                            />
+
                         </motion.div>
+
                     )
                 }
                 </LayoutGroup>
+
             </AnimatePresence>
+
+
         </article>
     )
-        {/*<p className={styles.mainContainer}>
-            <AnimatePresence>
-                {
-                    crumbs.map((item, index)=>{
-                        <Crumb
-                            key={`breadCrumb_${index}`}
-                            crumb={item}
-                            deleteCrumb={(crumb)=> removeGroup(crumb)}
-                        />
-                    })
-                    
-                }
-            </AnimatePresence>
-                {/*crumbs.map((item, index)=>
-                    <Crumb
-                        crumb={item}
-                        key={`breadCrumb_${index}`}
-                        deleteCrumb={(crumb)=> removeGroup(crumb)}
-                    />
-                )*}
-            
-        </p>*/}
-    //);
 }
