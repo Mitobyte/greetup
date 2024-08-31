19180da65949ce429643e50f131057da6d1ac8ad
Optiq01 <Optiqvision@gmail.com>
2024-08-31 02:05:58 -0500

    remove unused prop and clean up code in EventList

--- web/src/components/event-list/EventList.js
+++ web/src/components/event-list/EventList.js
@@ -1,20 +1,25 @@
-import * as React from 'react';
-import * as styles from './EventList.module.css';
-import {Location} from './location/Location';
+import * as React        from 'react';
+import * as styles       from './EventList.module.css';
+import {Location}        from './location/Location';
 import { DateComponent } from './date/DateComponent';
 
 
 
-export const EventList = ( { data, key } ) => {
+export const EventList = ( { data } ) => {
 
     return(
         
         <ul className={ styles.mainContainer }>
+
+
             {
                 data.map( ( item, index ) => {
 
                     return(
+
                         <li className={ styles.listItem } key={ `event_${ index }` }>
+
+
 
                             <h3 className={ `srcryTxt ${ styles.eventName }` }>{ item.name }</h3>
 
@@ -22,18 +27,25 @@ export const EventList = ( { data, key } ) => {
 
                             <DateComponent start={ item.startDate } end={ item.endDate } />
 
+
                             <a
-                                className={ `srcryTxt ${ styles.joinButton }`}
+                                className={ `srcryTxt ${ styles.joinButton }` }
                                 href={ item.url }
                                 target="_blank"
                             >
                                 Join Event
                             </a>
 
+
+
                         </li>
+
                     );
                 })
             }
+
+
         </ul>
+        
     );
 }
