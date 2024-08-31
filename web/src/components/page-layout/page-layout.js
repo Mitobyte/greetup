d4175b4ed59284caf85c0fdddee500ab89a46013
Optiq01 <Optiqvision@gmail.com>
2024-08-31 01:28:35 -0500

    remove un used prop and clean up code

--- web/src/components/page-layout/page-layout.js
+++ web/src/components/page-layout/page-layout.js
@@ -1,23 +1,30 @@
-import * as React from 'react';
-import './page-layout.css';
+import * as React     from 'react';
+import                './page-layout.css';
 import { SvgSymbols } from '../svg_symbols/SvgSymbols';
-import { NavBar } from '../navbar/NavBar';
-import { Footer } from '../footer/Footer';
+import { NavBar }     from '../navbar/NavBar';
+import { Footer }     from '../footer/Footer';
 
-export const PageLayout = ({data, children})=>{
+export const PageLayout = ( { children } ) => {
+
     return(
+
         <section>
+            
             <SvgSymbols />
 
-            <article className="navBarContainer">
-                <NavBar />
-            </article>
 
-            <main className="main">
-                {children}
-            </main>
+
+            <article className="navBarContainer"> <NavBar /> </article>
+
+
+
+            <main className="main"> { children } </main>
+
+
 
             <Footer />
+            
         </section>
     );
+
 }
