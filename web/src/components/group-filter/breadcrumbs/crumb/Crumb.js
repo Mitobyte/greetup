864a946ab14ba10a84fa5f8951ad3574e0b09fa9
Optiq01 <Optiqvision@gmail.com>
2024-08-31 02:16:37 -0500

    clean up code in crumb component

--- web/src/components/group-filter/breadcrumbs/crumb/Crumb.js
+++ web/src/components/group-filter/breadcrumbs/crumb/Crumb.js
@@ -1,13 +1,32 @@
-import * as React from 'react';
+import * as React  from 'react';
 import * as styles from './Crumb.module.css';
 
-export const Crumb = ({crumb, deleteCrumb}) => {
+
+
+
+
+
+
+export const Crumb = ( { crumb, deleteCrumb } ) => {
+
     return(
-        <span className={`srcryBox ${styles.crumbContainer}`}>
-            {crumb}
-            <svg className={styles.closeButton} viewBox="0 0 20 20" width="50%" onClick={()=> deleteCrumb(crumb)}>
-                <use href={'#close_button'} />
+
+        <span className={ `srcryBox ${ styles.crumbContainer }` }>
+
+            { crumb }
+
+            <svg
+                className={ styles.closeButton }
+                viewBox="0 0 20 20"
+                width="50%"
+                onClick={ () => deleteCrumb( crumb ) }
+            >
+
+                <use href={ '#close_button' } />
+
             </svg>
+
         </span>
+
     );
 }
