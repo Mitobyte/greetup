369dccaf76415fd7200e45421642dd8aedb8c6d8
Optiq01 <Optiqvision@gmail.com>
2024-08-31 02:58:46 -0500

    clean and comment code in search bar component

--- web/src/components/group-filter/search-bar/SearchBar.js
+++ web/src/components/group-filter/search-bar/SearchBar.js
@@ -1,45 +1,79 @@
-import * as React from 'react';
+import * as React  from 'react';
 import * as styles from './SearchBar.module.css';
 
-export const SearchBar = ({userInput, searchText})=>{
+
+
+
+
+
+
+export const SearchBar = ( { userInput, searchText } ) => {
     // userInput  : passes search text up to parent for narrowing down matches
     // searchText : passes current search text to parent to find exact match
 
-    const [userText, setUserText] = React.useState('');
 
+
+      //  Stores text as user types --->
+    const [ userText, setUserText ] = React.useState('');
+
+
+
+
+
+      //  Triggers a search when user pushes enter key --->
     const detectEnterKey = (event)=>{
         
         if(event.key === 'Enter'){ handleSearch(); }
-    }
-
-    const handleKeyStrokes = (value) => {
-
-        setUserText(value);
-        userInput(value);
 
     }
 
+
+
+      //  Updates userText as user types and passes
+      //  value up to parent for filtering --->
+    const handleKeyStrokes = ( value ) => {
+
+        setUserText( value );
+        userInput( value );
+
+    }
+
+
+    
+      //  Passes user text up to parent --->
     const handleSearch = () => { searchText(userText); }
 
+
+
+
+
     return(
+
         <article className={styles.mainContainer}>
+
+
             <label for="searchBar" className={`srcryBox ${styles.searchLabel}`}>
+
                 <input
                     id="searchBar"
                     type="search"
-                    className={styles.searchBar}
+                    className={ styles.searchBar }
                     placeholder="search for groups"
-                    onKeyUp={(event)=> detectEnterKey(event)}
-                    onChange={(event)=> handleKeyStrokes(event.target.value)}
+                    onKeyUp={ ( event ) => detectEnterKey( event ) }
+                    onChange={ ( event ) => handleKeyStrokes( event.target.value ) }
                 />
+
                 <button
                     type="button"
-                    className={styles.searchButton}
-                    onClick={handleSearch}
+                    className={ styles.searchButton }
+                    onClick={ handleSearch }
                 >
                     search
                 </button>
+
             </label>
+
+
         </article>
     );
 }
