c8b5e5a6828aad7700ba1a734fb124e476cbb6ae
Optiq01 <Optiqvision@gmail.com>
2024-09-05 20:14:54 -0500

    create 404 page

---
+++ web/src/pages/404.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { PageLayout } from '../components/page-layout/page-layout';
+
+
+
+
+
+
+
+export default function custom404Page() {
+
+
+
+    return(
+        <PageLayout>
+            <p>page not found</p>
+        </PageLayout>
+    );
+}
