2a4060a97efba02db1ec431579cf6f9dc175fff2
Optiq01 <Optiqvision@gmail.com>
2024-09-06 00:48:20 -0500

    add copyright text to footer

--- web/src/components/footer/Footer.js
+++ web/src/components/footer/Footer.js
@@ -1,10 +1,22 @@
-import * as React from 'react';
+import * as React  from 'react';
 import * as styles from './Footer.module.css';
 
-export const Footer = ()=>{
+export const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+
+
+
+
     return(
+
         <footer>
-            <p>footer</p>
+
+            <p className={`srcryTxt ${styles.copyright}`}>
+                mketechmeetups.com &#169; 2022&#45;{ currentYear } All rights reserved.
+            </p>
+
         </footer>
+        
     );
 }
