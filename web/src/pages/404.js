62e5b69e375d7eb5d12237549d10f79e0f180fb5
Optiq01 <Optiqvision@gmail.com>
2024-09-05 20:16:38 -0500

    create and add stylesheet for 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import * as React     from 'react';
+import * as styles    from './404.module.css';
 import { PageLayout } from '../components/page-layout/page-layout';
 
 
